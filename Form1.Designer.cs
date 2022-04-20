6315443248a430b70ef6fb9ccf6769e73e058c6c
DonJCann <donjcann@gmail.com>
2022-04-20 05:58:09 +0300

    Update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -161,7 +161,7 @@ namespace CourseProject_CISS_311
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "welcomeForm";
             this.Text = "Welcome";
-            this.Load += new System.EventHandler(this.welcomeForm_Load);
+
             this.ResumeLayout(false);
             this.PerformLayout();
 
