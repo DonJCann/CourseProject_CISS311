719ef89097653111f4292c2b71c012aaf2207bb8
afaf mahmood <afafmahmood48@gmail.com>
2022-04-24 16:15:57 -0600

    Add Course changes

--- AddCourseForm.Designer.cs
+++ AddCourseForm.Designer.cs
@@ -30,17 +30,22 @@ namespace CourseProject_CISS_311
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.courseNameTextBox = new System.Windows.Forms.TextBox();
+            this.courseSemesterComboBox = new System.Windows.Forms.ComboBox();
             this.addCourse = new System.Windows.Forms.Button();
             this.closeButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.courseIdTextBox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.courseIdTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.courseNameTextBox);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -49,39 +54,45 @@ namespace CourseProject_CISS_311
             this.groupBox1.Size = new System.Drawing.Size(396, 219);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Enter Course Data";            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 61);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Course Title:";
+            this.groupBox1.Text = "Enter Course Data";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(38, 151);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.Size = new System.Drawing.Size(145, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Semester Offered:";
+            this.label2.Text = "Course Semester:";
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(405, 112);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name:";
             // 
-            // comboBox1
+            // courseNameTextBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(405, 207);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 3;
+            this.courseNameTextBox.Location = new System.Drawing.Point(245, 96);
+            this.courseNameTextBox.Name = "courseNameTextBox";
+            this.courseNameTextBox.Size = new System.Drawing.Size(100, 26);
+            this.courseNameTextBox.TabIndex = 2;
+            // 
+            // courseSemesterComboBox
+            // 
+            this.courseSemesterComboBox.FormattingEnabled = true;
+            this.courseSemesterComboBox.Items.AddRange(new object[] {
+            "Fall",
+            "Spring",
+            "Winter",
+            "Summer"});
+            this.courseSemesterComboBox.Location = new System.Drawing.Point(405, 207);
+            this.courseSemesterComboBox.Name = "courseSemesterComboBox";
+            this.courseSemesterComboBox.Size = new System.Drawing.Size(121, 24);
+            this.courseSemesterComboBox.TabIndex = 3;
             // 
             // addCourse
             // 
@@ -103,13 +114,28 @@ namespace CourseProject_CISS_311
             this.closeButton.Text = "Cl&ose";
             this.closeButton.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Course Id:";
+            // 
+            // courseIdTextBox
+            // 
+            this.courseIdTextBox.Location = new System.Drawing.Point(245, 48);
+            this.courseIdTextBox.Name = "courseIdTextBox";
+            this.courseIdTextBox.Size = new System.Drawing.Size(100, 26);
+            this.courseIdTextBox.TabIndex = 4;
+            // 
             // AddCourseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.courseSemesterComboBox);
             this.Controls.Add(this.addCourse);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.groupBox1);
@@ -118,7 +144,6 @@ namespace CourseProject_CISS_311
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -127,9 +152,11 @@ namespace CourseProject_CISS_311
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox courseNameTextBox;
+        private System.Windows.Forms.ComboBox courseSemesterComboBox;
         private System.Windows.Forms.Button addCourse;
         private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox courseIdTextBox;
+        private System.Windows.Forms.Label label3;
     }
 }
--- AddCourseForm.cs
+++ AddCourseForm.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace CourseProject_CISS_311
 {
     public partial class AddCourseForm : Form
     {
+        // String variable at the class level so that all methods in the class can use it
+
+        string connectionString;
+
+        // A SqlConnection variable called conn declared which will be used in several methods of the class
+
+        SqlConnection conn;
         public AddCourseForm()
         {
             InitializeComponent();
+
+            // To assign a value to the variable declared
+
+            connectionString =
+                ConfigurationManager.ConnectionStrings
+                ["CourseProject_CISS_311.Properties.Settings.CourseProjectDBConnectionString"]
+                .ConnectionString;
+        }
+
+
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            //Closes the form
+            Close();
+        }
+
+        private void addCourse_Click_1(object sender, EventArgs e)
+        {
+            using (conn = new SqlConnection(connectionString))
+            using (SqlCommand comd = new SqlCommand
+
+            // SQL INSERT statement issued 
+            ("INSERT INTO courses (courseId, courseName, courseSemester)" +
+            "VALUES (@courseId, @courseName, @courseSemester)", conn))
+            {
+                // Connection opened because SqlDataAdapter is not used this time
+                conn.Open();
+                comd.Parameters.AddWithValue("@courseId",
+                    courseIdTextBox.Text);
+                comd.Parameters.AddWithValue("@courseName",
+                    courseNameTextBox.Text);
+                comd.Parameters.AddWithValue("@courseSemester",
+                    courseSemesterComboBox.SelectedValue);
+
+                // SQL INSERT statement executed
+                comd.ExecuteScalar();
+
+                //Displays that the course has been added
+                MessageBox.Show("Course Added.");
+            }
         }
     }
 }
